4e75126a29af2d102eb99273173224903500e24d
Pedro Henrique F. Dias <pedroarapua@gmail.com>
2019-01-16 03:46:13 -0200

    created authentication filter

--- backend/src/main/java/br/com/magazineluiza/v1/customers/security/AMSAuthenticationTokenFilter.java
+++ backend/src/main/java/br/com/magazineluiza/v1/customers/security/AMSAuthenticationTokenFilter.java
@@ -3,12 +3,20 @@ package br.com.magazineluiza.v1.customers.security;
 
 //import com.inner.satisfaction.backend.authentication.token.AuthenticationTokenService;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.UUID;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
@@ -16,12 +24,12 @@ import org.springframework.web.filter.GenericFilterBean;
 @Component
 public class AMSAuthenticationTokenFilter extends GenericFilterBean {
 
-  //private AuthenticationTokenService authenticationTokenService;
+  private JwtTokenProvider jwtTokenProvider;
 
-//  public AMSAuthenticationTokenFilter(
-//    AuthenticationTokenService authenticationTokenService) {
-//    this.authenticationTokenService = authenticationTokenService;
-//  }
+  public AMSAuthenticationTokenFilter(
+		  JwtTokenProvider jwtTokenProvider) {
+    this.jwtTokenProvider = jwtTokenProvider;
+  }
 
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
@@ -35,6 +43,18 @@ public class AMSAuthenticationTokenFilter extends GenericFilterBean {
     } else {
       token = Optional.empty();
     }
+    
+//    String token = jwtTokenProvider.resolveToken(req);
+    try {
+      if (jwtTokenProvider.validateToken(token.get())) {
+    	  SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(token, null, Collections.emptyList()));
+
+      }
+    } catch (AccessDeniedException ex) {
+      //this is very important, since it guarantees the user is not authenticated at all
+      //SecurityContextHolder.clearContext();
+    }
+
 
     
 //    Optional<AuthenticationToken> authenticationToken = token
